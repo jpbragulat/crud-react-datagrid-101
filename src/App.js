fec79808250cd42d96d3bd060446b5b41bfd2066
jpbragulat <juliopbragulat@gmail.com>
2023-08-23 21:21:49 -0300

    modficacion con paginas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import './App.css';
 import DataGridTest from './Table/DataGridTest';
-
-
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Container, Nav, NavItem } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-      <div className="App">
-        <h2>FLIGHT SCHOOL</h2>
-        <DataGridTest />
-        
-      </div>
-    );
-  }
+    <Router>
+      <Container className="mt-4">
+        <h2 className="text-center">FLIGHT SCHOOL</h2>
+        <Nav variant="tabs" className="mb-4">
+          <NavItem>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/data-grid" className="nav-link">
+              DataGridTest
+            </Link>
+          </NavItem>
+          {/* Add more buttons for other components */}
+        </Nav>
+
+        <Routes>
+          <Route path="/data-grid" element={<DataGridTest />} />
+          {/* Add routes for other components */}
+        </Routes>
+      </Container>
+    </Router>
+  );
+}
+
+
+
+
+
+//function App() {
+//  return (
+//      <div className="App">
+//        <h2>FLIGHT SCHOOL</h2>
+//        <DataGridTest />
+//        
+//      </div>
+//    );
+//  }
 
 
 export default App;
